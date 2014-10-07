5b706adad204c2bbd91a1985d8920d5c7b569dd6
Thomas Bechtold <tbechtold@suse.com>
2014-10-07 14:14:23 +0200

    Use region from keystone settings
    The keystone barclamp got the possibility to set a custom region
    name. This name must be used by other barclamps as well.
    
    related to: https://bugzilla.novell.com/show_bug.cgi?id=896481
    
    (cherry picked from commit 5c259254fd5448d3074feaf9ba730005bf24d7ec)

--- chef/cookbooks/heat/recipes/server.rb
+++ chef/cookbooks/heat/recipes/server.rb
@@ -215,7 +215,7 @@ keystone_register "register heat Cfn endpoint" do
   port keystone_settings['admin_port']
   token keystone_settings['admin_token']
   endpoint_service "heat-cfn"
-  endpoint_region "RegionOne"
+  endpoint_region keystone_settings['endpoint_region']
   endpoint_publicURL "#{node[:heat][:api][:protocol]}://#{my_public_host}:#{node[:heat][:api][:cfn_port]}/v1"
   endpoint_adminURL "#{node[:heat][:api][:protocol]}://#{my_admin_host}:#{node[:heat][:api][:cfn_port]}/v1"
   endpoint_internalURL "#{node[:heat][:api][:protocol]}://#{my_admin_host}:#{node[:heat][:api][:cfn_port]}/v1"
@@ -242,7 +242,7 @@ keystone_register "register heat endpoint" do
   port keystone_settings['admin_port']
   token keystone_settings['admin_token']
   endpoint_service "heat"
-  endpoint_region "RegionOne"
+  endpoint_region keystone_settings['endpoint_region']
   endpoint_publicURL "#{node[:heat][:api][:protocol]}://#{my_public_host}:#{node[:heat][:api][:port]}/v1/$(tenant_id)s"
   endpoint_adminURL "#{node[:heat][:api][:protocol]}://#{my_admin_host}:#{node[:heat][:api][:port]}/v1/$(tenant_id)s"
   endpoint_internalURL "#{node[:heat][:api][:protocol]}://#{my_admin_host}:#{node[:heat][:api][:port]}/v1/$(tenant_id)s"
